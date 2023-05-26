2196258fd5dd250a28a20ba613020c2a3e3f97a4
ramity <lewisclbrown@gmail.com>
2023-05-26 19:28:33 +0000

    added linux and windows pull and compile scripts for cryptopp

---
+++ scripts/libs/compile-l64-cryptopp.sh
@@ -0,0 +1,37 @@
+DIR="/root/iris/libs/src/cryptopp"
+VER="CRYPTOPP_8_7_0"
+CXX="g++"
+CXXFLAGS="-m64"
+PREFIX="/root/iris/libs/linux/crytopp"
+
+# create prefix dir if it does not exist
+if ! [ -d "$PREFIX" ]
+then
+    mkdir -p $PREFIX
+fi
+
+# clone dir if it does not exist
+if ! [ -d "$DIR" ]
+then
+    git clone https://github.com/weidai11/cryptopp.git $DIR
+fi
+
+cd $DIR
+
+# conditionally create branch if it does not exist and checkout specific tag
+if git rev-parse --quiet --verify "refs/tags/$tag" > /dev/null
+then
+    # branch exists, checkout
+    git checkout $VER
+else
+    # branch does not exist, checkout and create
+    git checkout tags/$VER -b $VER
+fi
+
+# compile cryptopp to prefix dir
+make -f GNUmakefile PREFIX=$PREFIX distclean
+make -f GNUmakefile PREFIX=$PREFIX CXX=$CXX CXXFLAGS=$CXXFLAGS -j`nproc`
+make -f GNUmakefile PREFIX=$PREFIX CXX=$CXX CXXFLAGS=$CXXFLAGS -j`nproc` install
+
+# This may or may not be required. Rebuilding container + testing required
+# sudo ldconfig
