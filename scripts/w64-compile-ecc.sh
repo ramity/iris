4112ecb56bf4bbae439212b76f01a6e66cd11c8b
ramity <lewisclbrown@gmail.com>
2023-05-27 02:32:59 +0000

    fixed w64-compile-ecc to point towards locally compiled windows cryptopp files

--- scripts/w64-compile-ecc.sh
+++ scripts/w64-compile-ecc.sh
@@ -1,8 +1,17 @@
-# Include directory
-INC="/root/iris/include/"
+INC="/root/iris/include"
+BIN="/root/iris/bin/win64-cc-test.exe"
+TGT="/root/iris/tests/crypto-ecc.cpp"
 
-# Target file
-TGT="/root/iris/tests/w64-crypto-ecc.cpp"
+CRYPTOPP_INC="/root/iris/libs/windows/cryptopp/include"
+CRYPTOPP_LIB="/root/iris/libs/windows/cryptopp/lib"
+
+WIN_64_CPL="x86_64-w64-mingw32-g++ -static-libgcc -static-libstdc++"
+
+# Remove bin file if it exists
+if [ -f "$BIN" ]
+then
+    rm $BIN
+fi
 
 # Exit if target file does not exist
 if ! [ -f "$TGT" ]
@@ -11,19 +20,12 @@ then
     exit 1
 fi
 
-# Windows 64-bit
-WIN_64_CPL="x86_64-w64-mingw32-g++ -static-libgcc -static-libstdc++"
-WIN_64_BIN="/root/iris/bin/win64-cc-test.exe"
-
-# Remove binaries if they exist
-if [ -f "$WIN_64_BIN" ]; then rm "$WIN_64_BIN"; fi
-
 # Compile windows 64-bit executable
 echo -e "\033[36m[C] Compiling\tWindows 64-bit\033[0m"
-$WIN_64_CPL -Wall $TGT -o $WIN_64_BIN -I /usr/local/include -L /usr/local/lib -I $INC -lcryptopp -static
-if [ -f "$WIN_64_BIN" ]
+$WIN_64_CPL -Wall $TGT -o $BIN -I $CRYPTOPP_INC -L $CRYPTOPP_LIB -I $INC -lcryptopp -static
+if [ -f "$BIN" ]
 then
-    echo -e "\033[32m[P] Compiled\t$TGT->$WIN_64_BIN\033[0m"
+    echo -e "\033[32m[P] Compiled\t$TGT->$BIN\033[0m"
 else
-    echo -e "\033[31m[C] Failed\t$TGT->$WIN_64_BIN\033[0m"
+    echo -e "\033[31m[C] Failed\t$TGT->$BIN\033[0m"
 fi
