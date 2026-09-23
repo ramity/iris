d7807bf62ede23e64fd11f7d6db93acad578f3f8
ramity <lewisclbrown@gmail.com>
2026-09-23 00:57:12 -0400

    updated provided test script to fully cleanup the created pub/pri keypairs

--- scripts/release/test.sh
+++ scripts/release/test.sh
@@ -2,7 +2,7 @@
 
 # Simply generate a keypair
 
-command="./iris keypair generate ./keys/pri_a ./keys/pub_a"
+command="./iris keypair generate ./keys/pri_a ./identities/pub_a"
 result=$(eval $command)
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
@@ -15,7 +15,7 @@ fi
 
 # Generate keypairs using the same seed and compare files
 
-command="./iris keypair generate ./keys/pri_b ./keys/pub_b \"UnlimitedGamesButNoGames\" 1"
+command="./iris keypair generate ./keys/pri_b ./identities/pub_b \"UnlimitedGamesButNoGames\" 1"
 result=$(eval $command)
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
@@ -26,7 +26,7 @@ else
     echo -e "\033[33m[$exit_code] '$command'\033[0m"
 fi
 
-command="./iris keypair generate ./keys/pri_c ./keys/pub_c \"UnlimitedGamesButNoGames\" 1"
+command="./iris keypair generate ./keys/pri_c ./identities/pub_c \"UnlimitedGamesButNoGames\" 1"
 result=$(eval $command)
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
@@ -43,15 +43,15 @@ else
     echo -e "\033[33m[F] ./keys/pri_b ./keys/pri_c match'\033[0m"
 fi
 
-if cmp -s "./keys/pub_b" "./keys/pub_c"; then
-    echo -e "\033[32m[P] ./keys/pub_b ./keys/pub_c match'\033[0m"
+if cmp -s "./identities/pub_b" "./identities/pub_c"; then
+    echo -e "\033[32m[P] ./identities/pub_b ./identities/pub_c match'\033[0m"
 else
-    echo -e "\033[33m[F] ./keys/pub_b ./keys/pub_c match'\033[0m"
+    echo -e "\033[33m[F] ./identities/pub_b ./identities/pub_c match'\033[0m"
 fi
 
-# Delete ./keys/pri_a and verify it was removed
+# Delete ./keys/pri_a and ./identities/pub_a and verify they were removed
 
-command="./iris keypair delete ./keys/pri_a ./keys/pub_a"
+command="./iris keypair delete ./keys/pri_a ./identities/pub_a"
 result=$(eval $command)
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
@@ -68,16 +68,16 @@ else
     echo -e "\033[33m[F] ./keys/pri_a was not removed'\033[0m"
 fi
 
-if [ ! -f "./keys/pub_a" ]; then
-    echo -e "\033[32m[P] ./keys/pub_a was removed'\033[0m"
+if [ ! -f "./identities/pub_a" ]; then
+    echo -e "\033[32m[P] ./identities/pub_a was removed'\033[0m"
 else
-    echo -e "\033[33m[F] ./keys/pub_a was not removed'\033[0m"
+    echo -e "\033[33m[F] ./identities/pub_a was not removed'\033[0m"
 fi
 
-# Encrypt using ./keys/pub_b
+# Encrypt using ./identities/pub_b
 
 plaintext="Hello, world!"
-command="./iris keypair encrypt ./keys/pub_b \"$plaintext\""
+command="./iris keypair encrypt ./identities/pub_b \"$plaintext\""
 ciphertext=$(eval $command)
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
@@ -111,7 +111,7 @@ fi
 
 # Add key pub_b to identities and verify the file contains the public key
 
-command="cat ./keys/pub_b"
+command="cat ./identities/pub_b"
 public_key=$(eval $command)
 command="./iris identity add ./identities/pub_b $public_key"
 result=$(eval $command)
@@ -189,4 +189,42 @@ elif [ $exit_code -eq 1 ]; then
     echo -e "\033[33m[F] '$command'\033[0m"
 else
     echo -e "\033[33m[$exit_code] '$command'\033[0m"
+fi
+
+# Remove private key via identity command
+
+command="./iris identity remove ./keys/pri_b"
+result=$(eval $command)
+exit_code=$?
+if [ $exit_code -eq 0 ]; then
+    echo -e "\033[32m[P] '$command'\033[0m"
+elif [ $exit_code -eq 1 ]; then
+    echo -e "\033[33m[F] '$command'\033[0m"
+else
+    echo -e "\033[33m[$exit_code] '$command'\033[0m"
+fi
+
+# Delete ./keys/pri_c and ./identities/pub_c and verify they were removed
+
+command="./iris keypair delete ./keys/pri_c ./identities/pub_c"
+result=$(eval $command)
+exit_code=$?
+if [ $exit_code -eq 0 ]; then
+    echo -e "\033[32m[P] '$command'\033[0m"
+elif [ $exit_code -eq 1 ]; then
+    echo -e "\033[33m[F] '$command'\033[0m"
+else
+    echo -e "\033[33m[$exit_code] '$command'\033[0m"
+fi
+
+if [ ! -f "./keys/pri_c" ]; then
+    echo -e "\033[32m[P] ./keys/pri_c was removed'\033[0m"
+else
+    echo -e "\033[33m[F] ./keys/pri_c was not removed'\033[0m"
+fi
+
+if [ ! -f "./identities/pub_c" ]; then
+    echo -e "\033[32m[P] ./identities/pub_c was removed'\033[0m"
+else
+    echo -e "\033[33m[F] ./identities/pub_c was not removed'\033[0m"
 fi
